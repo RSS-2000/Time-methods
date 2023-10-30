0b27d233de64bdb95e344dd70fffced5668f5ad7
Sergey_js15 <serj.777.front@gmail.com>
2023-10-30 15:23:38 +0200

    default + hours in localStorage

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ const pauseTimer = document.getElementById('buttonTimerPause');
 const stopTimer = document.getElementById('buttonTimerStop');
 const sound1 = document.querySelector('.sound1');
 
+in_Out_Timer.placeholder = `${localStorage.getItem('hour')}:${localStorage.getItem('minute')}:${localStorage.getItem('second')}`
 let timerSetInt;
 startTimer.addEventListener('click', (e) => {
     let timerInArr = in_Out_Timer.value.split(':');
-    let hour = +timerInArr[0];
-    let minute = +timerInArr[1];
-    let second = +timerInArr[2];
+    let inputValid = /^\d{2}:\d{2}:\d{2}$/.test(in_Out_Timer.value);
+    if (inputValid) {
+        localStorage.setItem('hour', timerInArr[0]);
+        localStorage.setItem('minute', timerInArr[1]);
+        localStorage.setItem('second', timerInArr[2]);
+    }
+
+    let hour = +localStorage.getItem('hour');
+    let minute = +localStorage.getItem('minute');
+    let second = +localStorage.getItem('second');
+
     if (!isNaN(hour) && !isNaN(minute) && !isNaN(second)) {
         timerSetInt = setInterval(() => {
             if (second > 0) {
@@ -26,17 +35,17 @@ startTimer.addEventListener('click', (e) => {
                     } else {
                         clearInterval(timerSetInt);
                         sound1.play();
-                        setTimeout(()=>{
+                        setTimeout(() => {
                             sound1.pause();
-                        },10000)
-                        
+                        }, 10000)
+
                     }
                 }
             }
             const formatHour = String(hour).padStart(2, '0');
             const formatMinute = String(minute).padStart(2, '0');
             const formatSecond = String(second).padStart(2, '0');
-            in_Out_Timer.value = `${formatHour}:${formatMinute}:${formatSecond}`
+            in_Out_Timer.value = `${formatHour}:${formatMinute}:${formatSecond}`;
         }, 1000)
     }
 
