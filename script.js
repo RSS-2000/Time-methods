6838fa32256e7d3e574c35134241b36fbce42ad2
Sergey_js15 <serj.777.front@gmail.com>
2023-10-31 22:37:19 +0200

    add stopwatch

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ const stopTimer = document.getElementById('buttonTimerStop');
 const sound1 = document.querySelector('.sound1');
 const imgTimer = document.getElementById('imgTimer');
 
-
-
-
 in_Out_Timer.placeholder = `${localStorage.getItem('hour')}:${localStorage.getItem('minute')}:${localStorage.getItem('second')}`;
 let timerSetInt;
 startTimer.addEventListener('click', (e) => {
@@ -19,23 +16,23 @@ startTimer.addEventListener('click', (e) => {
         localStorage.setItem('second', timerInArr[2]);
     }
 
-    let hour = +localStorage.getItem('hour');
-    let minute = +localStorage.getItem('minute');
-    let second = +localStorage.getItem('second');
+    let hourTimer = +localStorage.getItem('hour');
+    let minuteTimer = +localStorage.getItem('minute');
+    let secondTimer = +localStorage.getItem('second');
 
-    if (!isNaN(hour) && !isNaN(minute) && !isNaN(second)) {
+    if (!isNaN(hourTimer) && !isNaN(minuteTimer) && !isNaN(secondTimer)) {
         timerSetInt = setInterval(() => {
-            if (second > 0) {
-                second--;
+            if (secondTimer > 0) {
+                secondTimer--;
             } else {
-                if (minute > 0) {
-                    minute--;
-                    second = 59;
+                if (minuteTimer > 0) {
+                    minuteTimer--;
+                    secondTimer = 59;
                 } else {
-                    if (hour > 0) {
-                        hour--;
-                        minute = 59;
-                        second = 59;
+                    if (hourTimer > 0) {
+                        hourTimer--;
+                        minuteTimer = 59;
+                        secondTimer = 59;
                     } else {
                         clearInterval(timerSetInt);
                         sound1.play();
@@ -43,9 +40,9 @@ startTimer.addEventListener('click', (e) => {
                     }
                 }
             }
-            const formatHour = String(hour).padStart(2, '0');
-            const formatMinute = String(minute).padStart(2, '0');
-            const formatSecond = String(second).padStart(2, '0');
+            const formatHour = String(hourTimer).padStart(2, '0');
+            const formatMinute = String(minuteTimer).padStart(2, '0');
+            const formatSecond = String(secondTimer).padStart(2, '0');
             in_Out_Timer.value = `${formatHour}:${formatMinute}:${formatSecond}`;
         }, 1000)
     }
@@ -62,4 +59,103 @@ stopTimer.addEventListener('click', (e) => {
     sound1.load();
     imgTimer.style.display = 'none';
     textClickTimer.style.display = 'none';
+})
+
+//alarm clock
+const in_Out_Alarm = document.getElementById('in_Out_Alarm');
+const btnAlarmOn = document.getElementById('buttonAlarmOn');
+const btnAlarmOff = document.getElementById('buttonAlarmOff');
+const imgOnAlarm = document.querySelector('.imgOn');
+const imgOffAlarm = document.querySelector('.imgOff');
+const imgAlarm = document.querySelector('.imgAlarm');
+const sound2 = document.querySelector('.sound2');
+
+btnAlarmOn.addEventListener('click', (e) => {
+    const inTimeAlarm = /^\d{2}:\d{2}:\d{2}$/.test(in_Out_Alarm.value);
+    let indicator = true;
+    let audioInterval;
+    imgOnAlarm.style.display = 'block';
+    imgOffAlarm.style.display = 'none';
+    function alarmFunc() {
+        const date = new Date();
+        const hourAlarm = String(date.getHours()).padStart(2, '0')
+        const minuteAlarm = String(date.getMinutes()).padStart(2, '0');
+        const secondAlarm = String(date.getSeconds()).padStart(2, '0');
+        const fullTime = `${hourAlarm}:${minuteAlarm}:${secondAlarm}`;
+        console.log('ведений час', in_Out_Alarm.value)
+        console.log('поточний час', fullTime)
+        if (!inTimeAlarm) {
+            console.log('error not correct format')
+            imgOnAlarm.style.display = 'none';
+            imgOffAlarm.style.display = 'block';
+            return
+        } else {
+            if (in_Out_Alarm.value === fullTime) {
+                audioInterval = setInterval(() => { sound2.play() }, 1000)
+                imgOnAlarm.style.display = 'none';
+                imgAlarm.style.display = 'block';
+            } else {
+                if (indicator) {
+                    setTimeout(alarmFunc, 1000);
+                }
+            }
+        }
+    }
+    alarmFunc();
+    btnAlarmOff.addEventListener('click', (e) => {
+        indicator = false;
+        clearInterval(audioInterval);
+        sound2.pause();
+        sound2.load();
+        imgOnAlarm.style.display = 'none';
+        imgOffAlarm.style.display = 'block';
+        imgAlarm.style.display = 'none';
+    })
+})
+
+
+//stopwatch
+const buttonWatchStart = document.getElementById('buttonWatchStart');
+const buttonWatchStop = document.getElementById('buttonWatchStop');
+const buttonWatchReset = document.getElementById('buttonWatchReset');
+const in_Out_stopWatch = document.getElementById('in_Out_stopWatch');
+let stopwatch;
+let indWatch = true;
+buttonWatchStart.addEventListener('click', (even) => {
+    let hourWatch = 0;
+    let minuteWatch = 0;
+    let secondWatch = 0;
+    let milisecondWatch = 0;
+    if (indWatch) {
+        stopwatch = setInterval(() => {
+            indWatch = false;
+            milisecondWatch += 1;
+            if (milisecondWatch === 999) {
+                milisecondWatch = 0;
+                secondWatch += 1;
+                if (secondWatch === 59) {
+                    secondWatch = 0;
+                    minuteWatch += 1;
+                    if (minuteWatch === 59) {
+                        minuteWatch = 0;
+                        hourWatch += 1;
+                    }
+                }
+            }
+            const outWatch = `${String(hourWatch).padStart(2, '0')}:${String(minuteWatch).padStart(2, '0')}:${String(secondWatch).padStart(2, '0')}:${String(milisecondWatch).padStart(3, '00')}`;
+            in_Out_stopWatch.value = outWatch;
+        }, 1)
+    }
+
+});
+
+buttonWatchStop.addEventListener('click', (even) => {
+    clearInterval(stopwatch);
+    indWatch = true;
+});
+
+buttonWatchReset.addEventListener('click', (even) => {
+    clearInterval(stopwatch);
+    in_Out_stopWatch.value = '00:00:00:0000';
+    indWatch = true;
 })
